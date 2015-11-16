ef18571ec89d0107c4dac043012a95934d4f6953
mrcheng0910 <mrcheng0910@gmail.com>
2015-11-16 19:18:28 +0800

    修改页面内容

--- static/js/svr/svr_performance.js
+++ static/js/svr/svr_performance.js
@@ -16,6 +16,16 @@ $(function() {
             pie: {
                 allowPointSelect: true,
                 cursor: 'pointer',
+                events:{
+                    click: function(e){
+                        if (e.point.name=='含有whois服务器'){
+                            alert("test");
+                        }
+                        else if(e.point.name=='不含有whois服务器'){
+                            exist_detail();
+                        }
+                    }
+                },
                 dataLabels: {
                     enabled: true,
                     format: '<b>{point.name}</b>: {point.percentage:.1f} %',
@@ -27,25 +37,24 @@ $(function() {
         },
         series: [{
             type: 'pie',
-            name: 'Browser share',
+            name: '百分比',
             data: [
-                ['含有域名whois', 45.0],
-                ['没有域名whois', 55.0]
+                {name: '含有whois服务器', y:145},
+                {name: '不含有whois服务器', y:555},
             ]
+
         }]
     });
 });
 
 // 详细信息
 function exist_detail() {
-    $('#btn_exist_detail').on('click', function() {
-        layer.open({
-            type: 2,
-            title: '含有域名whois服务器域名',
-            shadeClose: true,
-            shade: 0.8,
-            area: ['66%', '75%'],
-            content: '/svr_table'
-        });
+    layer.open({
+        type: 2,
+        title: '含有域名whois服务器域名',
+        shadeClose: true,
+        shade: 0.8,
+        area: ['66%', '75%'],
+        content: '/svr_table'
     });
 };
