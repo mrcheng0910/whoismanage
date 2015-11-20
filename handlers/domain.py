e1bec60c9b2eaaba465be5a2d54105452101cdaa
mrcheng0910 <mrcheng0910@gmail.com>
2015-11-20 10:58:07 +0800

    添加了仪器表

--- handlers/domain.py
+++ handlers/domain.py
@@ -2,14 +2,23 @@
 import tornado
 from models.domain_db import DomainDb
 
+PATH = './domain/'
 
-class DomainHandler(tornado.web.RequestHandler):
+class DomainIndexHandler(tornado.web.RequestHandler):
     """各个顶级后缀域名数量统计"""
 
     def get(self):
         domains, total = DomainDb().get_domain(10)
-        self.render('domain.html',
+        self.render(PATH+'domain_index.html',
                     title_name="顶级后缀统计",
                     domains=domains,
                     total=total
+                    )
+
+
+class DomainQueryHandler(tornado.web.RequestHandler):
+    """域名相关查询"""
+    def get(self):
+        self.render(PATH+'domain_query.html',
+                    title_name = "域名查询"
                     )
--- urls.py
+++ urls.py
@@ -3,16 +3,18 @@
 系统路由设置
 """
 from handlers.index import IndexHandler
-from handlers.domain import DomainHandler
+from handlers.domain import DomainIndexHandler
 from handlers.domain_geo import DomainGeoHandler
+from handlers.domain import DomainQueryHandler
 from handlers.svr import *
 from handlers.tld import TldHandler
 
 urls = [
     (r'/', IndexHandler),
     (r'/tld', TldHandler),
-    (r'/domain_geography', DomainGeoHandler),
-    (r'/domain', DomainHandler), # 域名首页
+    (r'/domain_geography', DomainGeoHandler), #域名地理位置查询
+    (r'/domain', DomainIndexHandler), # 域名首页
+    (r'/domain_query',DomainQueryHandler),   # 域名查询
     (r'/svr', DomainSvrHandler), # 域名whois服务器首页
     (r'/svr_geo',SvrGeoHandler),  # 域名whois服务器地理位置
     (r'/svr_performance',SvrPerformanceHandler), # whois服务器性能
