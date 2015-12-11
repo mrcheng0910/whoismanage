4f9262f6736f6935624b69a4a5a4bf731ffd384e
mrcheng0910 <mrcheng0910@gmail.com>
2015-12-11 15:47:43 +0800

    优化代码

--- static/js/domain_whois/detect.js
+++ static/js/domain_whois/detect.js
@@ -1,36 +1,38 @@
+//初始化页面以及绑定函数
 $(function () {
-    var raw_data;
-    var categories =[]
-    var series_total = []
-    var series_increase = []
-    $.ajax({
-            url: '/detect/increase',
-            type: "get",
-            timeout: 5000, //超时时间
-            success: function (data) {  //成功后的处理
-                raw_data = JSON.parse(data);
-                for(var i=0,arrLength=raw_data.length;i<arrLength;i++){
-                    var value = raw_data[i]
-                    categories.unshift(value.insert_time.slice(11,value.insert_time.length)); //添加时间，截取详细时间
-                    series_total.unshift((Math.round((value.sum/1000000.0)*1000)/1000)); //添加数量，两位小数，百万级别
-                }
-                for(var i =1;i<series_total.length;i++){
-                    var single_value = (series_total[i]-series_total[i-1])/series_total[i-1];
-                    series_increase.push(Math.round(single_value*10000)/10000);
-                }
-                init(categories.slice(1,categories.length),series_total.slice(1,series_total.length),series_increase);
-            },
-            error: function (xhr) {
-                if (xhr.status == "0") {
-                    alert("超时，稍后重试");
-                } else {
-                    alert("错误提示：" + xhr.status + " " + xhr.statusText);
-                }
-            } // 出错后的处理
-        });
+    //初始化页面
+    $('#datetimepicker-start').datetimepicker({
+        format: "YYYY-MM-DD",
+        defaultDate: new Date(),
+        showTodayButton: true
+    });
+    $('#datetimepicker-end').datetimepicker({
+        useCurrent: false, //Important! See issue #1075
+        format: "YYYY-MM-DD",
+        defaultDate: new Date(),
+        showTodayButton: true
+    });
+    $("#datetimepicker-start").on("dp.change", function (e) {
+        $('#datetimepicker-end').data("DateTimePicker").minDate(e.date);
+    });
+    $("#datetimepicker-end").on("dp.change", function (e) {
+        $('#datetimepicker-start').data("DateTimePicker").maxDate(e.date);
+    });
+    
+    var start = $("#start_date").val(); //获取初始化后页面的起始日期
+    var end = $("#end_date").val(); //获取初始化后页面的终止日期
+    
+    get_data(start,end);  //初始化趋势
+    
+    $("#query").bind('click',function(){  //为查询按钮绑定查询函数
+        start = $("#start_date").val();
+        end = $("#end_date").val();
+        get_data(start,end);
+    });
 });
 
 function init(categories,series_total,series_increase){
+    //更新数据趋势
     $("#detect_period").text('探测时间段为：'+categories[0]+'至'+categories[categories.length-1]);
     $('#container').highcharts({
         credits: {
@@ -87,7 +89,6 @@ function init(categories,series_total,series_increase){
             max: series_total[series_total.length-1],
             alignTicks: false,
             gridLineWidth: 0
-            // max: null
         }],
         tooltip: {
             shared: true
@@ -122,13 +123,12 @@ function init(categories,series_total,series_increase){
     });
 }
 
-function test() {
+function get_data(start,end) {
+    //ajax获取最新统计数据，并更新页面
     var raw_data;
     var categories =[];
     var series_total = [];
     var series_increase = [];
-    var start = $("#start_date").val();
-    var end = $("#end_date").val();
     var flag=false;
     if (start==end){
         flag = true;
@@ -168,4 +168,4 @@ function test() {
                 }
             } // 出错后的处理
         });
-};
+}
