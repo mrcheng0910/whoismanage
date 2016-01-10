8b3c3c49c8504a70f421dd5cdca8e92af314c2ae
mrcheng0910 <mrcheng0910@gmail.com>
2016-01-10 16:58:05 +0800

    使用collections模块

--- database/tasks/update_domain.py
+++ database/tasks/update_domain.py
@@ -1,8 +1,10 @@
-#!/usr/bin/python
 # encoding:utf-8
+
 """
 功能：统计数据库中最新的域名数据domain_summary,domain_update两个表。
 作者：程亚楠
+更新：2016.1.10（程亚楠）
+使用了collections模块，提高程序效率以及可读性
 更新：2015.12.28
 """
 
@@ -10,6 +12,7 @@ import time
 import threading
 from threading import Thread
 from Queue import Queue
+from collections import defaultdict
 from datetime import datetime
 from data_base import MySQL
 from config import DESTINATION_CONFIG
@@ -17,43 +20,39 @@ from config import DESTINATION_CONFIG
 
 num_thread = 5      # 线程数量
 queue = Queue()     # 任务队列，存储sql
-sum_domains = []    # 存储域名的和
-lock = threading.Lock()
+lock = threading.Lock()  # 变量锁
+sum_domains = defaultdict(list)  # 存储所有表的合字典
 
 
-def sum_all_domains(single_domains=[]):
-    """合并探测的表到全局表中
-    :param single_domains:当前探测的表
-    :return:
+def merge_same_tld(tb_tld_num):
     """
+    合并所有表中相同tld到字典列表中
+    :param tb_tld_num:Tuple 当前表中各个tld的内容
+    """
+    for item, value in tb_tld_num:
+        sum_domains[item].append(value)
 
-    del_index = []      # 用来存放待删除的index
-    for index_sum, value_sum in enumerate(sum_domains):
-        for index_single, value_single in enumerate(single_domains):
-            if value_sum[0] == value_single[0]:
-                sum_domains.append(
-                    (value_sum[0], value_single[1] + value_sum[1]))
-                del_index.append(index_sum)
-                del single_domains[index_single]  # 删除
 
-    sum_domains.extend(single_domains)  # 将次列表中未删除的添加
-    del_index.reverse()  # 反向求解,比较巧妙,防止删除前面项后，导致错位
-    for i in del_index:  # 删除重复的项
-        del sum_domains[i]
+def sum_all_domains():
+    """计算各个tld的域名数量"""
+    tld_num = []
+    for item in sum_domains:
+        tld_num.append((item,sum(sum_domains[item])))
+    return tld_num
 
 
 def count_domain():
     """
-
+    计算域名的数量
     :param q:线程编号
     :param queue: 任务队列
     :return:
     """
     while 1:
         tb_name = queue.get()
-        single_domains = get_resource_data(tb_name)
+        tb_tld_num = get_resource_data(tb_name)
         lock.acquire()  # 锁
-        sum_all_domains(list(single_domains))
+        merge_same_tld(tb_tld_num)
         lock.release()  # 解锁
         queue.task_done()
         time.sleep(1)  # 去掉偶尔会出现错误
@@ -66,7 +65,7 @@ def get_resource_data(tb_name):
     :return: results: 查询结果
     """
     db = MySQL(DESTINATION_CONFIG)
-    db.query ('SELECT tld, sum(whois_sum) AS count FROM domain_whois_%s  GROUP BY tld' % tb_name)
+    db.query('SELECT tld, sum(whois_sum) AS count FROM domain_whois_%s  GROUP BY tld' % tb_name)
     results = db.fetchAllRows()
     db.close()
     return results
@@ -74,13 +73,14 @@ def get_resource_data(tb_name):
 
 def update_db():
     """更新数据库"""
+    tld_num = sum_all_domains()
     db = MySQL(DESTINATION_CONFIG)
     db.truncate('TRUNCATE TABLE domain_summary')
     update_sql = 'INSERT INTO domain_update(tld_name, domain_num) VALUES("%s", "%s")'
     summary_sql = 'INSERT INTO domain_summary(tld_name, domain_num) VALUES("%s", "%s")'
-    for domain in sum_domains:
-        db.insert_no_commit(update_sql % (domain[0],domain[1]))
-        db.insert_no_commit(summary_sql % (domain[0],domain[1]))
+    for tld,num in tld_num:
+        db.insert_no_commit(update_sql % (tld,num))
+        db.insert_no_commit(summary_sql % (tld,num))
     db.commit()
     db.close()
 
@@ -106,10 +106,10 @@ def count_tld_domains():
     """主操作"""
     print str(datetime.now()), '开始统计数据库中域名数量'
     global sum_domains
-    sum_domains = []  # 务必添加，初始化，否则会一直累加
+    sum_domains = defaultdict(list)  # 务必添加，初始化，否则会一直累加
     create_queue()
     create_thread()
     update_db()
     print str(datetime.now()), '结束统计数据库中域名数量'
 
-# count_tld_domains()
+count_tld_domains()
